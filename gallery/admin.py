7b61560af91854c504a7077384dcd0f98eb66e88
mohammadrezaaghaei1998 <mohammadrezaaghaei1998@yahoo.com>
2025-12-03 06:35:43 +0400

    added model name to admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category,GalleryPicture,Video,MostViewdVideos
+from .models import Category,GalleryPicture,Video,MostViewdVideos,GalleryItem
 
 admin.site.register(Category)
 admin.site.register(GalleryPicture)
 admin.site.register(Video)
 admin.site.register(MostViewdVideos)
+admin.site.register(GalleryItem)
